8e8b3be44a7c64306edffd54669315f4882a8263
evillalona19 <evillalona@hotmail.com>
2024-08-21 11:52:31 -0400

    changes

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express as pe 
 
-st.header('Ventas de Vehiculos') #crea encabezado
+st.header('Panel de Control Ventas de Vehiculos') #crea encabezado
 
 data = pd.read_csv('/Users/EVILLALONA/Phyton Basico/Proyecto Sprint 5/my_proyect/notebooks/vehicles_us.csv') # lee el conjunto de datos
-boton_h = st.button('Contruye Histograma') # crea un boton para histograma
-boton_d = st.button('Contruye Grafico Dispersion') # crea un boton para grafico dispersion
+boton_h = st.checkbox('Construye Histograma') # crea un boton para histograma
+boton_d = st.checkbox('Construye Grafico Dispersion') # crea un boton para grafico dispersion
 
 if boton_h:
     st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches') 
